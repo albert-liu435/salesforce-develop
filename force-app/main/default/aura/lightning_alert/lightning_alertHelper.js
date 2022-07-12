e2d79e92faf585f3e50da4337ae922d37c9f10f4
liuxili01 <liuxili@bestseller.com.cn>
2022-07-12 17:55:21 +0800

    lightning alert & lightning avatar

---
+++ force-app/main/default/aura/lightning_alert/lightning_alertHelper.js
@@ -0,0 +1,11 @@
+({
+    openAlert: function(cmp, event) {
+        this.LightningAlert.open({
+            message: 'this is the alert message',
+            theme: 'error',
+            label: 'Error!',
+        }).then(function() {
+            console.log('alert is closed');
+        });
+    }
+});
